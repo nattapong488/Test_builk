4fc6e5b0896c24b81a2bba3e4a29b6b0e5a60c10
james <nattapong_soponwattana@hotmail.com>
2022-07-06 15:28:51 +0700

    Complete version

--- game24/index.js
+++ game24/index.js
@@ -1,83 +1,72 @@
-// input number
-const number1 = document.getElementById("input-1");
-const number2 = document.getElementById("input-2");
-const number3 = document.getElementById("input-3");
-const number4 = document.getElementById("input-4");
+let oper = ["+", "-", "*", "/"];
+let oper2 = ["+", "-", "*", "/"];
+let oper3 = ["+", "-", "*", "/"];
 
-// show number
-const show1 = document.getElementById("show-1");
-const show2 = document.getElementById("show-2");
-const show3 = document.getElementById("show-3");
-const show4 = document.getElementById("show-4");
-
-// button
-const btnCalculate = document.getElementById("btn");
-const result = document.getElementById("result");
-let n1 = [];
-let n2 = [];
-let n3 = [];
-let n4 = [];
-
-function Input1(val) {
-  n1 = parseInt(val);
-  show1.innerHTML = parseInt(val);
+let num1 = "";
+let num2 = "";
+let num3 = "";
+let num4 = "";
+function store1(val) {
+  num1 = val;
+}
+function store2(val) {
+  num2 = val;
+}
+function store3(val) {
+  num3 = val;
+}
+function store4(val) {
+  num4 = val;
 }
 
-function Input2(val) {
-  n2 = parseInt(val);
-  show2.innerHTML = parseInt(val);
-}
-function Input3(val) {
-  n3 = parseInt(val);
-  show3.innerHTML = parseInt(val);
-}
-function Input4(val) {
-  n4 = parseInt(val);
-  show4.innerHTML = parseInt(val);
-}
+// let arr1 = oper.map((item) => {
+//   return item;
+// });
 
-function recieveInput() {
-  if (
-    n1 !== n2 &&
-    n1 !== n3 &&
-    n1 !== n4 &&
-    n2 !== n3 &&
-    n2 !== n4 &&
-    n3 !== n4
-  ) {
-    let ga = [];
-    // result.innerHTML = `${n1} + ${n2} + ${n3} + ${n4}`;
-    result.innerHTML = cal24(n1, n2, n3, n4);
-    // console.log(n1 + n2 + n3 + n4);
-  } else {
-    number1.value = "";
-    number2.value = "";
-    number3.value = "";
-    number4.value = "";
-
-    show1.innerHTML = 1;
-    show2.innerHTML = 2;
-    show3.innerHTML = 3;
-    show4.innerHTML = 4;
-
-    result.innerHTML = " ";
-    alert("ตัวเลขซ้ำกัน ใส่ตัวเลขใหม่อีกรอบ");
+// console.log(arr1);
+let keep = "";
+let checkInput = false;
+function cal() {
+  let text = "";
+  try {
+    for (let i = 0; i < oper.length; i++) {
+      for (let j = 0; j < oper2.length; j++) {
+        for (let k = 0; k < oper3.length; k++) {
+          text = ` ${num1} ${oper[i]} ${num2}  ${oper[j]} ${num3} ${oper[k]} ${num4}`;
+          let func = eval(text);
+          // console.log(func);
+          if (func === 24) {
+            document.getElementById("message").innerHTML += text + "<br>";
+            checkInput = true;
+          }
+        }
+      }
+      // keep = num1 + oper[i] +num2;
+    }
+  } catch (error) {
+    document.getElementById("message").innerHTML =
+      "eror Program can't calculate";
   }
-}
 
-function cal24(num1, num2, num3, num4) {
-  if (num1 * num2 + num3 + num4 === 24) {
-    return `${num1} * ${num2} + ${num3} + ${num4}`
-  } else if (num1 * num2 + num3 - num4 === 24) {
-    return  `${num1} * ${num2} + ${num3} - ${num4}`
-  } else if (num1 * num2 - num3 + num4 === 24) {
-    return  `${num1} * ${num2} - ${num3} + ${num4}`
-  }else if (num1 * num2 - num3 - num4 === 24) {
-    return  `${num1}*${num2}-${num3}-${num4}`
+  function check() {
+    if (checkInput == false) {
+      document.getElementById("message").innerHTML = "";
+      alert("ตัวเลขชุดนี้ไม่สามารถทำให้ผลลัพธ์กลายเป็น 24 ได้");
+    }
   }
-   else {
-    // result.style.alignItems="center"
-    return "ชุดนี้ไม่สามารถทำให้ผลลัพธ์กลายเป็น 24 ได้";
+
+  function reset() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("num3").value = "";
+    document.getElementById("num4").value = "";
+    checkInput = false;
   }
-  // console.log(num1,num2,num3,num4)
+
+  check();
+  reset();
+
+  // console.log(text);
+  // console.log(num1);
+  // document.getElementById("message").innerHTML = func;
 }
